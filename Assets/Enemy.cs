71b150cf1827d9aacfc8f1adc4f92c42a755cefb
jackliu1001 <liujack1001@yahoo.com>
2020-09-12 11:30:32 -0700

    false1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxhealth = 100;
+    int currentHealth;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+    }
+}
---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackPoint;
+
+    public float attackRange = 0.45f;
+    public int attackDamage = 25;
+    public LayerMask enemyLayer;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+        void Attack()
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+            
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+        }
+    }
+}
